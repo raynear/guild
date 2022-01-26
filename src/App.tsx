3830ae305d964807ab871c9f142e1c237b6c73cf
Noh Byungkuk <raynear@gmail.com>
2022-01-26 09:08:40 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
         return (
               <Routes>
-                <Route path="/" element={Home} />
-                <Route path="/about" element={About} />
-</Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/about" element={<About/>} />
+              </Routes>
         );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { cav } from '../util/caver';
+
+declare global {
+    interface Window {
+        klaytn:any;
+    }
+}
 
 const Home = () => {
+	const [account, setAccount] = useState('');
+
+	useEffect( () => {
+		if(typeof window.klaytn !== undefined) {
+			const klaytn = window['klaytn']
+
+			klaytn.on('accountsChanged', (accounts:any) => {
+				setAccount(accounts[0]);
+			  });
+
+			if(klaytn._kaikas.isEnabled() === false) {
+				try {
+					klaytn.enable().then((accounts:any) => {
+						setAccount(accounts[0]);
+
+						console.log(cav.account);
+
+						// caver.wallet.signMessage(accounts[0], 'test', caver.wallet.keyring.role.roleTransactionKey).then((signature:any) => {
+						// 	console.log(signature);
+						// });
+					})
+				  } catch (error) {
+					console.error(error)
+				  }
+			}
+		}
+	  }, [account]);
     return (
         <div>
             <h2>
-                홈
+                {account}
             </h2>
         </div>
     );
